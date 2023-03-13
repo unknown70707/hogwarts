d0a9b766b7a9b0e9aefaa9571c2e1c2c753536de
707 <4uliarahma@gmail.com>
2023-03-13 12:48:15 +0700

    update

---
+++ script.js
@@ -0,0 +1,23 @@
+let calcScrollvalue = () => {
+    let scrollProgress = document.getElementById
+    ("progress");
+    let progressValue = document.getElementById
+    ("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = 
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient (#f0c75e ${scrollValue}%, #d7d7d7 %{scrollValue}%)`;
+};
+
+window.onscroll = calcScrollvalue;
+window.onload = calcScrollvalue;
